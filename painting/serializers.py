b567409a3c2ce6617b6172f2a19671729e5fef2b
mahdi fathian <mahdi_fathian@comp.iust.ac.ir>
2024-11-14 17:24:25 +0330

    update painting crud
    update painting crud

--- painting/serializers.py
+++ painting/serializers.py
@@ -5,6 +5,7 @@ class PaintingDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Painting
         fields = ['painting_id', 'title', 'description', 'creation_date', 'artist']
+        read_only_fields = ['artist'] 
 
 class PaintingListSerializer(serializers.ModelSerializer):
     class Meta:
