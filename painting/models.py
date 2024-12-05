87fd2d9ee840abe3949164b85bca37ca6d6d1d4a
mahdi fathian <mahdi_fathian@comp.iust.ac.ir>
2024-12-05 15:34:57 +0330

    add fields to painting
    add fields to painting

--- painting/models.py
+++ painting/models.py
@@ -12,7 +12,16 @@ class Painting(models.Model):
     image = models.ImageField(upload_to='paintings/', null=True, blank=True)
     creation_date = models.DateField(auto_now_add=True)
     artist = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='paintings',default=None)
-    price = models.IntegerField(null=True, blank=True) 
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True) 
+    year = models.PositiveIntegerField(null=True, blank=True)
+    vertical_depth = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    horizontal_depth = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    style = models.CharField(max_length=255, null=True, blank=True)
+    material = models.CharField(max_length=255, null=True, blank=True)
+    
+
+
+
 
     def __str__(self):
         return self.title
--- painting/serializers.py
+++ painting/serializers.py
@@ -5,13 +5,13 @@ from .models import Like
 class PaintingDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Painting
-        fields = ['painting_id', 'title', 'description','image', 'creation_date', 'artist' , 'price']
+        fields = ['painting_id', 'title', 'description','image', 'creation_date', 'artist' , 'price', 'material' , 'style' , 'year' , 'vertical_depth' , 'horizontal_depth']
         read_only_fields = ['artist', 'creation_date'] 
 
 class PaintingListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Painting
-        fields = ['painting_id', 'title', 'description' ,'image' , 'creation_date' , 'price']
+        fields = ['painting_id', 'title', 'description' ,'image' , 'creation_date' , 'price','material' , 'style' , 'year' , 'vertical_depth' , 'horizontal_depth']
 
 
 class LikeSerializer(serializers.ModelSerializer):
